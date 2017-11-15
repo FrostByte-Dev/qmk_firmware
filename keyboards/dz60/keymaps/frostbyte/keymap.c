9fbf5950cf05522926ae0c2fe769b4c6c53834aa
Jacques Levasseur <frostbyte-gaming@outlook.com>
2017-11-14 22:14:37 -0500

    Swapped menu key to FN key in case of non split rshft.

--- keyboards/dz60/keymaps/frostbyte/keymap.c
+++ keyboards/dz60/keymaps/frostbyte/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,  \
       MO(_NAV),KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  \
       KC_LSFT, ______,  KC_Z,    KC_X, KC_C, KC_V,   KC_B, KC_N, KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(_FN), \
-      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,     KC_SPC,    KC_SPC,  KC_RALT, KC_RGUI, KC_NO,   KC_MENU, KC_RCTL \
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,     KC_SPC,    KC_SPC,  KC_RALT, KC_RGUI, KC_NO,   MO(_FN), KC_RCTL \
       ),
 
 /* NAV Layer
